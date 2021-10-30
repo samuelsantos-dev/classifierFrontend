3f4ade6fcf6dfa476e2902fab4fe4c552e4f164f
Samuel Santos <samuel.santos@tgid.com.br>
2021-10-30 15:24:02 -0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserFormComponent } from './users/components/user-form/user-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/users/users.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserFormComponent } from './components/user-form/user-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsersModule { }
